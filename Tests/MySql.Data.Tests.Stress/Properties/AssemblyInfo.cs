71cd2e1d785f14f79ea0a1d0dd8fb5255324d879
Nicholi <nschell@gmail.com>
2014-11-17 13:51:37 -0800

    Update EntityFramework to 6.1.1. Fixes for minor warnings. Also App.config fixes for Tests (not tested yet).

--- Tests/MySql.Data.Tests.Stress/Properties/AssemblyInfo.cs
+++ Tests/MySql.Data.Tests.Stress/Properties/AssemblyInfo.cs
@@ -56,4 +56,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+#pragma warning disable 1699
 [assembly: AssemblyKeyName("ConnectorNet")]
+#pragma warning restore 1699
